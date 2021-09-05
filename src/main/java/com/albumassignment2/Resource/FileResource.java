a557904b88eee332dd296ee2b04848f8d41b1369
Bhavya Shah <https://github.com/bhavyashah9612>
2021-09-05 15:08:59 -0400

    Final Fifth Commit

--- src/main/java/com/albumassignment2/Resource/FileResource.java
+++ src/main/java/com/albumassignment2/Resource/FileResource.java
@@ -1,9 +1,50 @@
 package com.albumassignment2.Resource;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.albumassignment2.service.FileService;
+import com.amazonaws.services.s3.Headers;
+import com.amazonaws.services.s3.model.S3Object;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @RestController
-@RequestMapping("")
+@RequestMapping("api/files")
 public class FileResource {
+
+    @Autowired
+    private FileService fileService;
+
+    @PostMapping
+    public boolean upload(@RequestParam(name = "file")MultipartFile file) {
+            return  fileService.upload(file);
+    }
+
+    @GetMapping("/view")
+    public void view(@RequestParam(name = "key") String key, HttpServletResponse response) throws IOException {
+       S3Object s3Object = fileService.getfile(key);
+       response.setContentType(s3Object.getObjectMetadata().getContentType());
+       response.getOutputStream().write(s3Object.getObjectContent().readAllBytes());
+    }
+
+    @GetMapping("/download")
+    public ResponseEntity<Resource> download(@RequestParam(name = "key")String key) throws IOException {
+        S3Object object = fileService.getfile(key);
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType(object.getObjectMetadata().getContentType()))
+                .header(Headers.CONTENT_DISPOSITION, "attachment; filename=\"" + key + "\"")
+                .body(new ByteArrayResource(object.getObjectContent().readAllBytes()));
+    }
+
+    @DeleteMapping("/delete")
+    public void delete(@RequestParam(name = "key") String key, HttpServletResponse response) {
+        fileService.deletefile(key);
+    }
 }
--- src/main/java/com/albumassignment2/service/FileService.java
+++ src/main/java/com/albumassignment2/service/FileService.java
@@ -1,4 +1,52 @@
 package com.albumassignment2.service;
 
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.S3Object;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+
+@Service
 public class FileService {
+
+    public boolean upload(MultipartFile file) {
+
+        Object file_path = "ss";
+        Object bucket_name = "amazefirstbucket";
+        System.out.format("Uploading %s to S3 bucket %s...\n", file_path, bucket_name);
+        BasicAWSCredentials basic = new BasicAWSCredentials("AKIAYOFIMBX653JNFDEL ","Az7U4GyOcBf7jUbg/P1T+NO1I9ySXSnvaVd4D5aG");
+        final AmazonS3 s3 = AmazonS3ClientBuilder.standard().withCredentials(new AWSStaticCredentialsProvider(basic)).withRegion(Regions.US_EAST_2).build();
+        try {
+            ObjectMetadata metadata = new ObjectMetadata();
+            metadata.setContentLength(file.getSize());
+            metadata.setContentType(file.getContentType());
+            s3.putObject("amazefirstbucket", file.getOriginalFilename(), file.getInputStream(), metadata);
+            return true;
+        } catch (AmazonServiceException | IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public S3Object getfile(String key) {
+        BasicAWSCredentials basic = new BasicAWSCredentials("AKIAYOFIMBX653JNFDEL ","Az7U4GyOcBf7jUbg/P1T+NO1I9ySXSnvaVd4D5aG");
+        final AmazonS3 s3 = AmazonS3ClientBuilder.standard().withCredentials(new AWSStaticCredentialsProvider(basic)).withRegion(Regions.US_EAST_2).build();
+        return s3.getObject("amazefirstbucket", key);
+    }
+
+    public void deletefile(String key) {
+        BasicAWSCredentials basic = new BasicAWSCredentials("AKIAYOFIMBX653JNFDEL ","Az7U4GyOcBf7jUbg/P1T+NO1I9ySXSnvaVd4D5aG");
+        final AmazonS3 s3 = AmazonS3ClientBuilder.standard().withCredentials(new AWSStaticCredentialsProvider(basic)).withRegion(Regions.US_EAST_2).build();
+         s3.deleteObject("amazefirstbucket", key);
+    }
 }
+
+
